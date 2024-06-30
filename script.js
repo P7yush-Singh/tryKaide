20349af51a21469e0bc1f49fe7c03f3e30fdd47b
P7yush-Singh <142720296+P7yush-Singh@users.noreply.github.com>
2024-06-30 10:29:10 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -22,7 +22,8 @@ gsap.fromTo(
       delay: 0.5,
     }
   );
-  
+
+
 
 
 window.addEventListener("scroll", function(){
@@ -39,4 +40,47 @@ function CloseBtn(){
     sidebar.style.display = 'none'
 }
 
+const acordino = document.querySelectorAll('.Acordino');
+
+acordino.forEach((Acordino => {
+  Acordino.addEventListener('click', () => {
+    Acordino.classList.toggle('active');
+  })
+}))
+
 // Scroll Reveal
+ScrollReveal().reveal(".A-h1,.Acordino, .A-sc, .F, .S, .plans", { delay:2, scale: 2, reset: true, rotate: {
+  x: 20,
+  y: 12,
+  z: 0
+} } );
+// Image Slider 
+$(".step").click( function() {
+	$(this).addClass("active").prevAll().addClass("active");
+	$(this).nextAll().removeClass("active");
+});
+
+$(".step01").click( function() {
+	$("#line-progress").css("width", "3%");
+	$(".discovery").addClass("active").siblings().removeClass("active");
+});
+
+$(".step02").click( function() {
+	$("#line-progress").css("width", "25%");
+	$(".strategy").addClass("active").siblings().removeClass("active");
+});
+
+$(".step03").click( function() {
+	$("#line-progress").css("width", "50%");
+	$(".creative").addClass("active").siblings().removeClass("active");
+});
+
+$(".step04").click( function() {
+	$("#line-progress").css("width", "75%");
+	$(".production").addClass("active").siblings().removeClass("active");
+});
+
+$(".step05").click( function() {
+	$("#line-progress").css("width", "100%");
+	$(".analysis").addClass("active").siblings().removeClass("active");
+});
